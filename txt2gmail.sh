896440135105ae94e1cce4b7e13cd2c5a38984d5
hohno-46466 <hohno.46466@gmail.com>
2019-08-05 17:34:19 +0900

    added by hohno

--- txt2gmail.sh
+++ txt2gmail.sh
@@ -2,16 +2,50 @@
 
 # Last update: Mon Aug  5 08:02:25 JST 2019
 
-USER_FROM=hohno.46466@gmail.com
+USER_FROM=example.99999@gmail.com
 
-USER_TO=hohno_46466@yahoo.co.jp
+USER_TO=example_99999@yahoo.co.jp
 SUBJECT="test mail"
 
-(cat << "-EOF-"
+msmtp=/usr/local/bin/msmtp
+
+while [ "x$1" != "x" ]; do
+  # echo "[ $1 ]"
+  if [ "x$1" = "x-s" -a "x$2" != "x" ]; then
+	SUBJECT="$2"
+	shift
+	shift
+	break;
+  else
+	break;
+  fi
+  shift
+done
+
+if [ "x$1" != "x" ]; then
+  USER_TO="$(echo $@ | sed 's/ /,/g')"
+fi
+
+echo ""
+echo "(From: $USER_FROM)"
+echo "(To: $USER_TO)"
+echo "(Subject: $SUBJECT)"
+echo ""
+
+[ -x $msmtp ] || exit 1
+
+(cat << -EOF-
 To: $USER_TO
 From: $USER_FROM
 Subject: $SUBJECT
 
 -EOF-
-cat "$@" ) \
-| msmtp -d -t
+cat ) \
+| $msmtp -d -t
+
+echo ""
+echo "(From: $USER_FROM)"
+echo "(To: $USER_TO)"
+echo "(Subject: $SUBJECT)"
+echo ""
+
