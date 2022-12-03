16b4b75500d7c152d74b0edae45a6b6ba98b0e7d
hohno-46466 <hohno.46466@gmail.com>
2022-12-03 17:33:49 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- txt2gmail.sh
+++ txt2gmail.sh
@@ -3,8 +3,8 @@
 # txt2gmail.sh
 
 # First version: Mon Aug  5 08:02:25 JST 2019
-# Prev updated: Tue Oct 29 14:36:34 JST 2019
-# Last updated: Fri Jun 10 05:33:06 JST 2022
+# Prev update: Tue Oct 29 14:36:34 JST 2019
+# Last update: Fri Jun 10 05:33:06 JST 2022
 
 # ----------------------------------------------------------
 
